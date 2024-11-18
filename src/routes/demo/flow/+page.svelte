474e03d1a098ae2d6cfe2e70ae91fa17856bf1db
cdans <ch.dansard@gmail.com>
2024-11-18 11:45:31 +0800

    prettier nodes

--- src/routes/demo/flow/+page.svelte
+++ src/routes/demo/flow/+page.svelte
@@ -1,6 +1,14 @@
 <script lang="ts">
 	import { writable } from 'svelte/store';
-	import { SvelteFlow, Controls, Background, BackgroundVariant, MiniMap } from '@xyflow/svelte';
+	import {
+		SvelteFlow,
+		Controls,
+		ControlButton,
+		Background,
+		BackgroundVariant,
+		MiniMap,
+		MarkerType
+	} from '@xyflow/svelte';
 
 	// 👇 this is important! You need to import the styles for Svelte Flow to work
 	import '@xyflow/svelte/dist/style.css';
@@ -10,29 +18,52 @@
 	// We are using writables for the nodes and edges to sync them easily. When a user drags a node for example, Svelte Flow updates its position.
 	const nodes = writable([
 		{
-			id: '1',
-			type: 'input',
-			data: { label: 'Input Node' },
+			id: 'A',
+			type: 'default',
+			data: { label: 'A' },
 			position: { x: 0, y: 0 }
 		},
 		{
-			id: '2',
+			id: 'B',
 			type: 'default',
-			data: { label: 'Node' },
-			position: { x: 0, y: 150 }
+			data: { label: 'B' },
+			position: { x: 250, y: 0 }
 		}
 	]);
 
 	// same for edges
 	const edges = writable([
 		{
-			id: '1-2',
+			id: 'A-B',
 			type: 'default',
-			source: '1',
-			target: '2',
-			label: 'Edge Text'
+			source: 'A',
+			target: 'B'
 		}
 	]);
+
+	const defaultEdgeOptions = {
+		type: 'floating',
+
+		markerEnd: {
+			type: MarkerType.ArrowClosed
+		}
+	};
+
+	const addNode = () => {
+		nodes.update((nodes) => [
+			...nodes,
+			{
+				id: `${String.fromCharCode(nodes.length + 65)}`,
+				type: 'default',
+				data: { label: `${String.fromCharCode(nodes.length + 65)}` },
+				// golden spiral based on fibonacci sequence, based on the number of nodes
+				position: {
+					x: 500,
+					y: (nodes.length - 2) * 100
+				}
+			}
+		]);
+	};
 
 	const snapGrid: [number, number] = [25, 25];
 </script>
@@ -46,11 +77,14 @@ This means that the parent container needs a height to render the flow.
 		{nodes}
 		{edges}
 		{snapGrid}
+		{defaultEdgeOptions}
 		colorMode={$darkMode ? 'dark' : 'light'}
 		fitView
 		on:nodeclick={(event) => console.log('on node click', event.detail.node)}
 	>
-		<Controls />
+		<Controls>
+			<ControlButton on:click={() => addNode()}>+</ControlButton>
+		</Controls>
 		<Background variant={BackgroundVariant.Dots} />
 		<MiniMap />
 	</SvelteFlow>
